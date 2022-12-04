6a404806b06026d8d854e9d7ef6d531d1fddefa9
ianator963 <ianator963@gmail.com>
2022-12-04 17:41:40 -0500

    creating user inputs for nmap rules

--- nmappy.py
+++ nmappy.py
@@ -5,6 +5,11 @@ nmappy
 print("<options>")
 
 #take user input
+#	- ports/ port range, IP address, nmap options
+
+what_ports = input(f'PORT / PORT RANGE:\n\t>>')
+what_ip = input(f'IP:\n\t>>')
+what_options = input('NMAP OPTIONS:\n\t>>')
 
 #initiate scan
 
