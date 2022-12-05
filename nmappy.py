b4fdfd54205c649cac67f7324444743ef64abf39
ianator963 <ianator963@gmail.com>
2022-12-04 21:08:29 -0500

    added return (im stupid) and made called command line function to print

--- nmappy.py
+++ nmappy.py
@@ -12,27 +12,32 @@ def helport():
 	if what_ports == "help":
 		print("Enter the port / ports you would like to scan: (ex. 23, 23-200)")
 		helport()
+	return what_ports
 
 def helpip():
 	what_ip = input(f'IP:\n\t>>')
 	if what_ip == "help":
 		print("Enter the ip address for which you would like to scan: (ex. 8.8.8.8)")
 		helpip()
+	return what_ip
 
 def helpopt():
 	what_options = input(f'NMAP OPTIONS:\n\t>>')
 	if what_options == "help":
 		print("Enter the additional opetions you would like to apply to your scan: (ex. -sL)")
 		helpopt()
+	return what_options
 
-what_ip = helpip()
-what_ports = helport()
-what_options = helpopt()
+target = str(helpip())
+t_ports = helport()
 
-nm = nmap.PortScanner()
+scanner = nmap.PortScanner()
 
-new_scan = nm.scan(what_ip, what_ports)
-print(new_scan)
+res = scanner.scan(target, t_ports)
+
+print(scanner.command_line())
+
+
 
 
 #initiate scan
